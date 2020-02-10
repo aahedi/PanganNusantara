c2ea5946563702b6bee81d2575b3b477ecceb446
aahedi <aahedi@gmail.com>
2020-02-10 13:53:53 +0700

    Add files via upload

--- www/function.js
+++ www/function.js
@@ -1,10 +1,10 @@
 var versi = "1";
-var url = "https://duta.web.id/app/";
-var url_admin = "https://duta.web.id/";
+var url = "https://sutraalam.com/app/";
+var url_admin = "https://sutraalam.com/";
 var api_ongkir = "https://ongkir.tokojs.com/api_ongkir/";
 
 var token='6KB4J3xGZX';
-var nama_aplikasi='DUTA UTAMA ELECTRONIC';
+var nama_aplikasi='Sutra Alam';
 var id_pelanggan=localStorage.getItem('id_pelanggan');
 
 var ua = navigator.userAgent.toLowerCase();
@@ -50,8 +50,7 @@ function menu_kiri() {
         $("#menu_kiri").append("<li style=\"list-style:none\"><a href=\"#informasi\"><i class=\"icon-info\"></i>Informasi</a></li>");
         $("#menu_kiri").append('<li style="list-style:none"><a href="javascript:;" onclick="logout()"><i class=\"icon-power\"></i>Logout</a></li>');
         $("#menu_kiri").append("<li style=\"list-style:none\"><a href=\"#kirim_pesan\"><i class=\"icon-mail\"></i>Kirim Pesan</a></li>");
-        $("#menu_kiri").append("<li style=\"list-style:none\"><a href=\"#saldo\"><i class=\"icon-credit-card\"></i>Saldo</a></li>");
-        $("#menu_kiri").append("<li style=\"list-style:none\"><a href=\"#point\"><i class=\"icon-chart\"></i>Point</a></li>");
+       //$("#menu_kiri").append("<li style=\"list-style:none\"><a href=\"#saldo\"><i class=\"icon-credit-card\"></i>Saldo</a></li>");
    
     }
     $("#menu_kiri").append("<li style=\"list-style:none\"><a href=\"#kontak\"><i class=\"icon-contacts\"></i>Kontak Kami</a></li>");
@@ -69,7 +68,7 @@ function menu_kanan() {
     $("#menu_kanan").append("<li style=\"list-style:none\"><a href=\"#page+3\"><i class=\"icon-lightbulb\"></i>Info Reseller</a></li>");
     $("#menu_kanan").append("<li style=\"list-style:none\"><a href=\"#rekening\"><i class=\"icon-tags\"></i>Info Rekening</a></li>");
     $("#menu_kanan").append("<li style=\"list-style:none\"><a href=\"#cek_ongkir\"><i class=\"icon-calculator\"></i>Cek Ongkir</a></li>");
-    $("#menu_kanan").append("<li style=\"list-style:none\"><a href=\"#cekresi\"><i class=\"icon-ticket\"></i>Cek Resi</a></li>");
+    $("#menu_kanan").append("<li style=\"list-style:none\"><a href=\"javascript:;\" onclick=\"window.open('http://cekresi.com', '_blank', 'location=no');\" ><i class=\"icon-ticket\"></i>Cek Resi</a></li>");
     $("#menu_kanan").append("<li style=\"list-style:none\"><a href=\"#page+2\"><i class=\"icon-star-filled\"></i>Testimoni</a></li>");
     $("#menu_kanan").append("<li style=\"list-style:none\"><a href=\"#update\"><i class=\"icon-upload-cloud\"></i>Update</a></li>");
     $("#menu_kanan").append("</ul>");
